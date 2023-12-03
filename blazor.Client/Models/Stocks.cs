4920f0d84b67318bd8bf6dfde226b034d3116105
Sushil Khatri <139630872+SuseelKc@users.noreply.github.com>
2023-12-03 14:56:20 +0545

    Validation of inputs

--- blazor.Client/Models/Stocks.cs
+++ blazor.Client/Models/Stocks.cs
@@ -1,16 +1,24 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace blazor.Client.Models;
 
 public class Stocks
 {
     public int ID { get; set; }
 
+    [Required]
+    [StringLength(100)]
     public required string CompanyName { get; set; }
 
+    [Required]
+    [StringLength(50)]
     public required string Sector { get; set; }
 
+    [Range(1, int.MaxValue)]
     public int PublicIssueShares { get; set; }
 
-    public int Rate { get; set; }
+    [Range(1, 999999999)]
+    public decimal Rate { get; set; }
 
     public DateTime OpenDate { get; set; }
 
